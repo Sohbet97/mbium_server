298dbe76ab932a995de9a4245104aa701c393e3b
Dovletli <102576879+Dowletli@users.noreply.github.com>
2026-05-14 09:02:48 +0500

    Readme

--- backend/config/app.js
+++ backend/config/app.js
@@ -26,7 +26,7 @@ const app = express();
 // const { createRedisClient, redisMiddleware, bindRedisClient } = require("../utils/redis/redis-client");
 
 // Uncomment this for set up cors security
-var allowlist = ["http://192.168.1.100", "http://localhost:3000", "http://localhost:3001"];
+var allowlist = ["http://localhost:3000"];
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions = {
     origin: false,
